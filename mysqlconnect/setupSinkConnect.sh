999a252642a0381e56905759f46d98389de747cc
Cliff Gilmore <cwg100@gmail.com>
2017-10-25 06:39:52 -0500

    Last commit before Workshop4

--- mysqlconnect/setupSinkConnect.sh
+++ mysqlconnect/setupSinkConnect.sh
@@ -1,1 +1,1 @@
-curl -s -X POST -H "Content-Type: application/json" --data @postgres-vip-poor-ratings.json http://localhost:8083/connectors
+curl -s -X POST -H "Content-Type: application/json" --data @/home/ksql/ksql/mysqlconnect/postgres-vip-poor-ratings.json http://localhost:8083/connectors
--- mysqlconnect/setupSourceConnect.sh
+++ mysqlconnect/setupSourceConnect.sh
@@ -1,2 +1,2 @@
-curl -s -X POST -H "Content-Type: application/json" --data @users-config.json http://localhost:8083/connectors
-curl -s -X POST -H "Content-Type: application/json" --data @routes-config.json http://localhost:8083/connectors
+curl -s -X POST -H "Content-Type: application/json" --data @/home/ksql/ksql/mysqlconnect/users-config.json http://localhost:8083/connectors
+curl -s -X POST -H "Content-Type: application/json" --data @/home/ksql/ksql/mysqlconnect/routes-config.json http://localhost:8083/connectors
