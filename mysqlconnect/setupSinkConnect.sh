8cb16b0ea8b17418bfd81f2ae5ebf239929d05e8
Cliff Gilmore <cwg100@gmail.com>
2017-10-25 06:34:01 -0500

    Last commit before Workshop2

--- mysqlconnect/setupSinkConnect.sh
+++ mysqlconnect/setupSinkConnect.sh
@@ -1,1 +1,1 @@
-curl -s -X POST -H "Content-Type: application/json" --data @postgres-vip-poor-ratings.json http://localhost:8083/connectors | jq
+curl -s -X POST -H "Content-Type: application/json" --data @postgres-vip-poor-ratings.json http://localhost:8083/connectors
