ddc2d34a717bb9e1a8213832e8d2755967cb418e
Cliff Gilmore <cwg100@gmail.com>
2017-10-25 06:36:56 -0500

    Last commit before Workshop3

--- mysqlconnect/setupSourceConnect.sh
+++ mysqlconnect/setupSourceConnect.sh
@@ -1,2 +1,2 @@
-curl -s -X POST -H "Content-Type: application/json" --data @users-config.json http://connect:8083/connectors
-curl -s -X POST -H "Content-Type: application/json" --data @routes-config.json http://connect:8083/connectors
+curl -s -X POST -H "Content-Type: application/json" --data @users-config.json http://localhost:8083/connectors
+curl -s -X POST -H "Content-Type: application/json" --data @routes-config.json http://localhost:8083/connectors
